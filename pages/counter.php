531c5e836960262e66a91f80ddca10f8922e6a81
Roman Klassen <klassen.roman@comp-gluk.ru>
2018-03-23 09:26:57 +0300

    Добавлен учет показаний счетчиков

--- pages/counter.php
+++ pages/counter.php
@@ -1,41 +1,48 @@
 <?php
 $counterid = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
-//if ($counterid>0)
-//{
-//}
-if (isset($_GET['action']))
+if ($counterid<=0)
 {
-	$action = $_GET['action'];
+	die("Плохой номер счетчика");
+}
+if (isset($_POST['action']))
+{
+	$action = $_POST['action'];
 	
 	switch($action)
 	{
 		case "delete":
-				$usercounter = dbSelect($dbconnect, "SELECT c.id, c.name FROM counters c INNER JOIN flats f ON c.flat_id = f.id WHERE f.user_id='".$userinfo['id']."' AND c.id=$counterid;");
-				if (count($usercounter)>0)
+				$usercounter = dbSelect($dbconnect, "SELECT cv.id as id, cv.value as value FROM counter_values cv INNER JOIN counters c ON c.id=cv.counter_id INNER JOIN flats f ON f.id=c.flat_id WHERE f.user_id='".$userinfo['id']."' AND counter_id='".$counterid."'");
+	
+				if (count($usercounter)>0 && isset($_POST['vaue_id']))
 				{
-					dbExecuteQuery($dbconnect, "DELETE FROM counters WHERE id='".$counterid."'");
+					$value_id = $dbconnect->real_escape_string($_POST['vaue_id']);
+					dbExecuteQuery($dbconnect, "DELETE FROM counter_values WHERE id='".$value_id."'");
 				}
 			break;
 			
 		case "add":
-			if (isset($_GET['name']) && isset($_GET['number']) && isset($_GET['flatid']))
+			if (isset($_POST['value']))
 			{
-				$name=$dbconnect->real_escape_string($_GET['name']);
-				$number=$dbconnect->real_escape_string($_GET['number']);
-				$flatid=$dbconnect->real_escape_string($_GET['flatid']);
-				dbExecuteQuery($dbconnect, "INSERT INTO counters (`name`,`number`,`flat_id`) VALUES('$name','$number','$flatid');");
+				$value=floatval($_POST['value']);
+				dbExecuteQuery($dbconnect, "INSERT INTO counter_values (`value`,`counter_id`, `date`) VALUES('$value','$counterid',NOW());");
 			}
 			break;
 	}
 }
 
-$counter_values = dbSelect($dbconnect, "SELECT cv.id as id, cv.value as value FROM counter_values cv INNER JOIN counters c ON c.id=cv.counter_id INNER JOIN flats f ON f.id=c.flat_id WHERE f.user_id='".$userinfo['id']."' AND counter_id='".$counterid."' ORDER BY cv.date;");
+$counter_values = dbSelect($dbconnect, "SELECT cv.id as id, cv.value as value, cv.date as date FROM counter_values cv INNER JOIN counters c ON c.id=cv.counter_id INNER JOIN flats f ON f.id=c.flat_id WHERE f.user_id='".$userinfo['id']."' AND counter_id='".$counterid."' ORDER BY cv.date;");
 
 ?>
 
 <div class="row">
-    <div class="col-lg-12">
+	<div class="col-lg-12">
+		<h1 class="page-header">Показания счетчика</h1>
+	</div>
+</div>
+
+<div class="row">
+    <div class="col-lg-6">
 		<div class="table-responsive">
 			<table class="table table-hover">
 				<thead>
@@ -50,7 +57,7 @@ $counter_values = dbSelect($dbconnect, "SELECT cv.id as id, cv.value as value FR
 
 foreach ($counter_values as $counter_value)
 {
-	echo "<tr><td>".$counter_value['date']."</td><td>".$counter_value['value']."</td><td><a href=\"index.php?page=counter&action=delete&id=".$counter_value['id']."\">Удалить</a></td></tr>";
+	echo "<tr><td>".$counter_value['date']."</td><td>".$counter_value['value']."</td><td><form action=\"index.php?page=counter&id=".$counterid."\" method=\"POST\"> <input type=\"hidden\" name=\"action\" value=\"delete\"> <input type=\"hidden\" name=\"vaue_id\" value=\"".$counter_value['id']."\"> <input type=\"submit\" name=\"submit\" value=\"Удалить\"></form></td></tr>";
 }
 
 ?>
@@ -63,14 +70,13 @@ foreach ($counter_values as $counter_value)
 
 <div class="row">
     <div class="col-lg-12">
-        <form role="form" method="GET" action="index.php" class="form-inline">
+        <form role="form" method="POST" action="index.php?page=counter&id=<?php echo $counterid; ?>" class="form-inline">
                                             <div class="form-group">
                                                 <label>Показание</label>
                                                 <input class="form-control" name="value">
                                             </div>
                                             <input type="submit" class="btn btn-primary" value="Добавить">
                                             <input type="hidden" name="page" value="counter">
-                                            <input type="hidden" name="id" value="<?php echo $counterid; ?>">
                                             <input type="hidden" name="action" value="add">
                                         </form>
     </div>
